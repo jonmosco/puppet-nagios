d33de8c26e7a67fbd1fbadb0c9492d08419fb816
Jon Mosco <jonny.mosco@gmail.com>
2013-11-16 22:18:57 -0500

    updated resources and target spec

--- manifests/host.pp
+++ manifests/host.pp
@@ -1,11 +1,18 @@
 # == Class:: nagios::host
 #
+#    This class contains the basic host monitoring:
+#    check_ping
+#    check_uptime
+#
+#    All host definitions will be stored in /etc/nagios/conf.d/
+#
 class nagios::host inherits nagios {
   @@nagios_host { $::fqdn:
     ensure  => present,
     alias   => $::hostname,
     address => $::ipaddress,
     use     => 'generic-host',
+    target  => "/etc/nagios/conf.d/nagios_host_${::fqdn}.cfg",
   }
 
   @@nagios_service { "check_ping_${::hostname}":
--- manifests/server.pp
+++ manifests/server.pp
@@ -8,10 +8,23 @@ class nagios::server inherits nagios {
   package { 'nagios-plugins':
     ensure => installed,
   }
+  file { '/etc/nagios/conf.d':
+    ensure => directory,
+    owner  => 'nagios',
+  }
   service { 'nagios':
     ensure     => running,
     enable     => true,
     hasrestart => true,
+    require    => Package['nagios'],
+  }
+
+  # collect resources and populate /etc/nagios/nagios_*.cfg
+  Nagios_host    <<||>> {
+  notify   => Service['nagios']
+  }
+  Nagios_service <<||>> {
+  notify => Service['nagios']
   }
 
 }
